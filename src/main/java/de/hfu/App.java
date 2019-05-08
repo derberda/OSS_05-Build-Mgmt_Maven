a8c8208081a0d696056b4cd8aae52a732c4965eb
Berdan Der <berdan.der@hs-furtwangen.de>
2019-05-08 19:28:53 +0200

    comments

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -2,18 +2,29 @@ package de.hfu;
 import java.util.Scanner;
 /**
  * Hello world!
- *
+ * Hallo :)
  */
 public class App 
 {
+	/**
+	 * Das ist Kommentar Nr1
+	 *
+	 */
     public static void main( String[] args )
     {
     	System.out.println("Input: ");
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 		System.out.println(input.toUpperCase());
+		/**
+		 * Das ist Kommentar Nr2
+		 *
+		 */
     }
 }
 
-
+/**
+ * Das ist Kommentar Nr1
+ *
+ */
 
