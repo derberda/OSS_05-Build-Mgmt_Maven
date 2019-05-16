c26daa038cab0556516e34957d1eb27da18e46fa
Berdan Der <berdan.der@hs-furtwangen.de>
2019-05-16 17:03:29 +0200

    EasyMock Test

--- src/test/java/de/hfu/IntegrationsTest.java
+++ src/test/java/de/hfu/IntegrationsTest.java
@@ -1,6 +1,7 @@
 package de.hfu;
 
 import static org.junit.Assert.*;
+import static org.easymock.EasyMock.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
@@ -18,175 +19,195 @@ import de.hfu.residents.service.ResidentServiceException;
 public class IntegrationsTest {
 
 	List<Resident> liste = new ArrayList<Resident>();
-    List<Resident> result = new ArrayList<Resident>();
-    List<Resident> result1 = new ArrayList<Resident>();
-    List<Resident> result2 = new ArrayList<Resident>();
-    
-    @Test 
-    public void getFilteredResidentsTest() {
-        
-        Resident r1 = new Resident();
-        r1.setGivenName("Stephan");
-        r1.setFamilyName("Mosbach");
-        r1.setStreet("Dammstrasse");
-        r1.setCity("Gosheim");
-        r1.setDateOfBirth(null);
-        
-        
-        Resident r2 = new Resident();
-        r2.setGivenName("Lilli");
-        r2.setFamilyName("Essmer");
-        r2.setStreet("Bondelstrasse");
-        r2.setCity("Brigachtal");
-        r2.setDateOfBirth(null);
-    
-        liste.add(r1);
-        liste.add(r2);
-        
-        ResidentRepositoryStub stub = new ResidentRepositoryStub(liste);        
-                    
-        
-        BaseResidentService service = new BaseResidentService();
-        service.setResidentRepository(stub);
-        
-        Resident filterResident = new Resident("L*","*","*","*", null);
-        Resident filterResident2 = new Resident("*","*","B*","*", null);
-        Resident filterResident3 = new Resident("*", "E*", "*", "*", null);
-                
-        result = service.getFilteredResidentsList(filterResident);
-        result1 = service.getFilteredResidentsList(filterResident2);
-        result2 = service.getFilteredResidentsList(filterResident3);
-        
-        
-        assertEquals(r2.getGivenName(), result.get(0).getGivenName());
-        assertEquals(r2.getStreet(), result1.get(0).getStreet());
-        assertEquals(r2.getFamilyName(), result2.get(0).getFamilyName());
-    }
-    
-    @Test 
-    public void getUniqueResidentsTest() throws ResidentServiceException {
-        
-        List<Resident> liste = new ArrayList<Resident>();
-        
-        Resident r1 = new Resident();
-        r1.setGivenName("Stephan");
-        r1.setFamilyName("Mosbach");
-        r1.setStreet("Dammstrasse");
-        r1.setCity("Gosheim");
-        r1.setDateOfBirth(null);
-        
-        
-        Resident r2 = new Resident();
-        r2.setGivenName("Lilli");
-        r2.setFamilyName("Essmer");
-        r2.setStreet("Bondelstrasse");
-        r2.setCity("Brigachtal");
-        r2.setDateOfBirth(null);
-        
-        liste.add(r1);
-        liste.add(r2);
- 
-        ResidentRepositoryStub stub = new ResidentRepositoryStub(liste);
-        
-        BaseResidentService service = new BaseResidentService();
-        service.setResidentRepository(stub);
-        
-        Resident UniqueResident = new Resident("Stephan","Mosbach","Dammstrasse","Gosheim", null);
-        Resident UniqueResident2 = new Resident("Lilli","Essmer","Bondelstrasse","Brigachtal", null);
-        Resident UniqueResident3 = new Resident("Lilli","Essmer","Bondelstrasse","Brigachtal", null);
-                
-        Resident result = service.getUniqueResident(UniqueResident);
-        Resident result1 = service.getUniqueResident(UniqueResident2);
-        Resident result2 = service.getUniqueResident(UniqueResident3);
-        
-        
-        assertEquals(r1.getGivenName(), result.getGivenName());
-        assertEquals(r2.getFamilyName(), result1.getFamilyName());
-        assertEquals(r2.getStreet(), result2.getStreet());
-        
+	List<Resident> result = new ArrayList<Resident>();
+	List<Resident> result1 = new ArrayList<Resident>();
+	List<Resident> result2 = new ArrayList<Resident>();
 
+	@Test
+	public void getFilteredResidentsTest() {
 
-            }
-            @Test
-            public void testGetFilteredResidentsList3()
-            {
-            List<Resident> list = new LinkedList<Resident>();
-            list.add(new Resident("Max", "Mustermann", "Musterstraße", "Musterstadt", new Date()));
-            list.add(new Resident("Martina", "Mustermann", "Albertstraße", "Musterdorf", new Date()));
-            list.add(new Resident("Emil", "Dick", "Waldweg", "Musterdorf", new Date()));
-            ResidentRepository stub = new ResidentRepositoryStub(list);
+		Resident r1 = new Resident();
+		r1.setGivenName("Stephan");
+		r1.setFamilyName("Mosbach");
+		r1.setStreet("Dammstrasse");
+		r1.setCity("Gosheim");
+		r1.setDateOfBirth(null);
 
-            ResidentService rs = new BaseResidentService();
-            ((BaseResidentService) rs).setResidentRepository(stub);
-            List<Resident> result = rs.getFilteredResidentsList(new Resident("Ma*", "Mu*", "*straße", "Muster*", new Date()));
+		Resident r2 = new Resident();
+		r2.setGivenName("Lilli");
+		r2.setFamilyName("Essmer");
+		r2.setStreet("Bondelstrasse");
+		r2.setCity("Brigachtal");
+		r2.setDateOfBirth(null);
 
-            assertEquals(2, result.size());
+		liste.add(r1);
+		liste.add(r2);
 
-            assertEquals("Max", result.get(0).getGivenName());
-            assertEquals("Mustermann", result.get(0).getFamilyName());
-            assertEquals("Musterstraße", result.get(0).getStreet());
-            assertEquals("Musterstadt", result.get(0).getCity());
-            assertEquals("Martina", result.get(1).getGivenName());
-            assertEquals("Mustermann", result.get(1).getFamilyName());
-            assertEquals("Albertstraße", result.get(1).getStreet());
-            assertEquals("Musterdorf", result.get(1).getCity());
-            }
-            @Test
-            public void testGetUniqueResident1()
-            {
-            List<Resident> list = new LinkedList<Resident>();
-            ResidentRepository stub = new ResidentRepositoryStub(list);
+		ResidentRepositoryStub stub = new ResidentRepositoryStub(liste);
 
-            ResidentService rs = new BaseResidentService();
-            ((BaseResidentService) rs).setResidentRepository(stub);
+		BaseResidentService service = new BaseResidentService();
+		service.setResidentRepository(stub);
 
-            try {
-            Resident result = rs.getUniqueResident(new Resident("Martina", "Mustermann", "Albertstraße", "Musterdorf", null));
-            fail("getUniqueResident on empty List should throw exception");
-            } catch (ResidentServiceException e) {
-            assertEquals("Suchanfrage lieferte kein eindeutiges Ergebnis!", e.getMessage());
-            }
-            }
-            @Test
-            public void testGetUniqueResident2()
-            {
-            List<Resident> list = new LinkedList<Resident>();
-            ResidentRepository stub = new ResidentRepositoryStub(list);
+		Resident filterResident = new Resident("L*", "*", "*", "*", null);
+		Resident filterResident2 = new Resident("*", "*", "B*", "*", null);
+		Resident filterResident3 = new Resident("*", "E*", "*", "*", null);
 
-            ResidentService rs = new BaseResidentService();
-            ((BaseResidentService) rs).setResidentRepository(stub);
+		result = service.getFilteredResidentsList(filterResident);
+		result1 = service.getFilteredResidentsList(filterResident2);
+		result2 = service.getFilteredResidentsList(filterResident3);
 
-            try {
-            Resident result = rs.getUniqueResident(new Resident("*", "*", "*", "*", null));
-            fail("getUniqueResident with should throw exception");
-            } catch (ResidentServiceException e) {
-            assertEquals("Wildcards (*) sind nicht erlaubt!", e.getMessage());
-            }
-            }
+		assertEquals(r2.getGivenName(), result.get(0).getGivenName());
+		assertEquals(r2.getStreet(), result1.get(0).getStreet());
+		assertEquals(r2.getFamilyName(), result2.get(0).getFamilyName());
+	}
 
+	@Test
+	public void getUniqueResidentsTest() throws ResidentServiceException {
 
-            @Test
-            public void testGetUniqueResident3()
-            {
-            List<Resident> list = new LinkedList<Resident>();
-            list.add(new Resident("Max", "Mustermann", "Musterstraße", "Musterstadt", new Date()));
-            list.add(new Resident("Martina", "Mustermann", "Albertstraße", "Musterdorf", new Date()));
-            list.add(new Resident("Emil", "Dick", "Waldweg", "Musterdorf", new Date()));
-            ResidentRepository stub = new ResidentRepositoryStub(list);
+		List<Resident> liste = new ArrayList<Resident>();
 
-            ResidentService rs = new BaseResidentService();
-            ((BaseResidentService) rs).setResidentRepository(stub);
+		Resident r1 = new Resident();
+		r1.setGivenName("Stephan");
+		r1.setFamilyName("Mosbach");
+		r1.setStreet("Dammstrasse");
+		r1.setCity("Gosheim");
+		r1.setDateOfBirth(null);
 
-            try {
-            Resident result = rs.getUniqueResident(new Resident("Max", "Mustermann", null, null, null));
-            assertEquals("Max", result.getGivenName());
-            assertEquals("Mustermann", result.getFamilyName());
-            assertEquals("Musterstraße", result.getStreet());
-            assertEquals("Musterstadt", result.getCity());
-            } catch (ResidentServiceException e) {
-            fail(e.getMessage());
-            e.printStackTrace();
-            }
-            }
+		Resident r2 = new Resident();
+		r2.setGivenName("Lilli");
+		r2.setFamilyName("Essmer");
+		r2.setStreet("Bondelstrasse");
+		r2.setCity("Brigachtal");
+		r2.setDateOfBirth(null);
 
+		liste.add(r1);
+		liste.add(r2);
+
+		ResidentRepositoryStub stub = new ResidentRepositoryStub(liste);
+
+		BaseResidentService service = new BaseResidentService();
+		service.setResidentRepository(stub);
+
+		Resident UniqueResident = new Resident("Stephan", "Mosbach", "Dammstrasse", "Gosheim", null);
+		Resident UniqueResident2 = new Resident("Lilli", "Essmer", "Bondelstrasse", "Brigachtal", null);
+		Resident UniqueResident3 = new Resident("Lilli", "Essmer", "Bondelstrasse", "Brigachtal", null);
+
+		Resident result = service.getUniqueResident(UniqueResident);
+		Resident result1 = service.getUniqueResident(UniqueResident2);
+		Resident result2 = service.getUniqueResident(UniqueResident3);
+
+		assertEquals(r1.getGivenName(), result.getGivenName());
+		assertEquals(r2.getFamilyName(), result1.getFamilyName());
+		assertEquals(r2.getStreet(), result2.getStreet());
+
+	}
+
+	@Test
+	public void testGetFilteredResidentsList3() {
+		List<Resident> list = new LinkedList<Resident>();
+		list.add(new Resident("Max", "Mustermann", "Musterstraße", "Musterstadt", new Date()));
+		list.add(new Resident("Martina", "Mustermann", "Albertstraße", "Musterdorf", new Date()));
+		list.add(new Resident("Emil", "Dick", "Waldweg", "Musterdorf", new Date()));
+		ResidentRepository stub = new ResidentRepositoryStub(list);
+
+		ResidentService rs = new BaseResidentService();
+		((BaseResidentService) rs).setResidentRepository(stub);
+		List<Resident> result = rs
+				.getFilteredResidentsList(new Resident("Ma*", "Mu*", "*straße", "Muster*", new Date()));
+
+		assertEquals(2, result.size());
+
+		assertEquals("Max", result.get(0).getGivenName());
+		assertEquals("Mustermann", result.get(0).getFamilyName());
+		assertEquals("Musterstraße", result.get(0).getStreet());
+		assertEquals("Musterstadt", result.get(0).getCity());
+		assertEquals("Martina", result.get(1).getGivenName());
+		assertEquals("Mustermann", result.get(1).getFamilyName());
+		assertEquals("Albertstraße", result.get(1).getStreet());
+		assertEquals("Musterdorf", result.get(1).getCity());
+	}
+
+	@Test
+	public void testGetUniqueResident1() {
+		List<Resident> list = new LinkedList<Resident>();
+		ResidentRepository stub = new ResidentRepositoryStub(list);
+
+		ResidentService rs = new BaseResidentService();
+		((BaseResidentService) rs).setResidentRepository(stub);
+
+		try {
+			Resident result = rs
+					.getUniqueResident(new Resident("Martina", "Mustermann", "Albertstraße", "Musterdorf", null));
+			fail("getUniqueResident on empty List should throw exception");
+		} catch (ResidentServiceException e) {
+			assertEquals("Suchanfrage lieferte kein eindeutiges Ergebnis!", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetUniqueResident2() {
+		List<Resident> list = new LinkedList<Resident>();
+		ResidentRepository stub = new ResidentRepositoryStub(list);
+
+		ResidentService rs = new BaseResidentService();
+		((BaseResidentService) rs).setResidentRepository(stub);
+
+		try {
+			Resident result = rs.getUniqueResident(new Resident("*", "*", "*", "*", null));
+			fail("getUniqueResident with should throw exception");
+		} catch (ResidentServiceException e) {
+			assertEquals("Wildcards (*) sind nicht erlaubt!", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetUniqueResident3() {
+		List<Resident> list = new LinkedList<Resident>();
+		list.add(new Resident("Max", "Mustermann", "Musterstraße", "Musterstadt", new Date()));
+		list.add(new Resident("Martina", "Mustermann", "Albertstraße", "Musterdorf", new Date()));
+		list.add(new Resident("Emil", "Dick", "Waldweg", "Musterdorf", new Date()));
+		ResidentRepository stub = new ResidentRepositoryStub(list);
+
+		ResidentService rs = new BaseResidentService();
+		((BaseResidentService) rs).setResidentRepository(stub);
+
+		try {
+			Resident result = rs.getUniqueResident(new Resident("Max", "Mustermann", null, null, null));
+			assertEquals("Max", result.getGivenName());
+			assertEquals("Mustermann", result.getFamilyName());
+			assertEquals("Musterstraße", result.getStreet());
+			assertEquals("Musterstadt", result.getCity());
+		} catch (ResidentServiceException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGetUniqueResidentMock() {
+		List<Resident> list = new LinkedList<Resident>();
+		list.add(new Resident("Max", "Mustermann", "Musterstraße", "Musterstadt", new Date()));
+		list.add(new Resident("Martina", "Mustermann", "Albertstraße", "Musterdorf", new Date()));
+		list.add(new Resident("Emil", "Dick", "Waldweg", "Musterdorf", new Date()));
+
+		ResidentRepository mock = createMock(ResidentRepository.class);
+		expect(mock.getResidents()).andReturn(list);
+		replay(mock);
+
+		ResidentService rs = new BaseResidentService();
+		((BaseResidentService) rs).setResidentRepository(mock);
+
+		try {
+			Resident result = rs.getUniqueResident(new Resident("Max", "Mustermann", null, null, null));
+			assertEquals("Max", result.getGivenName());
+			assertEquals("Mustermann", result.getFamilyName());
+			assertEquals("Musterstraße", result.getStreet());
+			assertEquals("Musterstadt", result.getCity());
+		} catch (ResidentServiceException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+
+		verify(mock);
+	}
 }
